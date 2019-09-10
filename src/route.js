cdcc612543b365c7e615d71c15c58512de3d38e0
Adam Lee <adam.lee@activatedresearch.com>
2019-09-10 10:26:32 -0500

    add logger as dependency on scheduler

--- src/route.js
+++ src/route.js
@@ -1,5 +1,8 @@
 const m = require('mithril');
+const logger = require('./logger/logger');
 const scheduler = require('./scheduler/scheduler');
+
+logger.info("starting application");
 
 m.route(document.body, '/', {
   '/': scheduler,
